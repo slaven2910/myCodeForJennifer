9529d52f2ea9e37d6d0370e6ae325595127e069b
Slaven Mlinaric <slaven2910@gmail.com>
2021-02-28 09:08:19 +0100

    initial commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+/* console.log("it works")
+
+$(document).ready(function () {
+    $(".submit").click(function (event) {
+        event.preventDefault()
+        console.log("Clicked a button")
+    })
+}) */
+/* not important, copied this from a site */
